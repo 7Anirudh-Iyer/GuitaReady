47ed2c0862985adccb8f7c8ed07fa3ef7b86dfa8
7Anirudh-Iyer <72651426+7Anirudh-Iyer@users.noreply.github.com>
2021-04-02 12:06:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,15 +1,43 @@
-const canvas = document.getElementById('c1')
-const c = canvas.getContext('2d')
-let a
+let yoohoo
+let i,final,done,x = 40,f
 
-console.log(window.innerWidth)
-
-function draw() {
-    canvas.width=window.innerWidth
-    canvas.height=window.innerHeight-100
-
-    c.strokeStyle = 'white'
-    a = c.strokeRect(0,0,canvas.width,canvas.height)
+function preload(){
+  final = loadSound("beat.mp3")
 }
 
-let callfunction = setInterval(draw, 100)
+function setup() {
+  createCanvas(windowWidth, 500);
+}
+
+function draw() {
+  background(220);
+
+  rectMode(CENTER)
+  line(40, windowHeight / 2, windowWidth - 40, windowHeight / 2)
+  
+  if(mouseIsPressed){
+    x = mouseX
+  }
+
+  map(x,0,100,0,10)
+  
+  i = x-40
+  f = 60/i
+  
+  rect(x, windowHeight / 2, 10,40)
+  text(i + 'bpm', windowWidth / 2, 100)
+  
+}
+
+function yayhoo(x, y, width, height) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+
+  rect(this.x, this.y, this.width, this.height)
+}
+
+var timerID = setInterval(function() {
+  console.log(f)  
+}, f * 1000);
